293ab9b6e6105c4ce20b6d24d5e43bed00cc89c1
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2024-08-17 16:51:08 +0000

    business logic done in user service

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,1 +1,36 @@
 package auth
+
+import (
+	"context"
+	"huma-auth/pkg/utils"
+	db "huma-auth/sql/sqlc"
+)
+
+type Service struct {
+	Repository *Repository
+}
+
+func NewService(Repository *Repository) *Service {
+	return &Service{
+		Repository: Repository,
+	}
+}
+
+func (svc *Service) RegisterUser(ctx context.Context, user User) (*db.User, error) {
+	//hash user password
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return nil, err
+	}
+	// pass the hashedPassword down to the repository
+	user.Password = hashedPassword
+	u, err := svc.Repository.CreateUser(ctx, user)
+	//return user
+	return &db.User{
+		ID:          u.ID,
+		Name:        u.Name,
+		Email:       u.Email,
+		PhoneNumber: u.PhoneNumber,
+		IsVerified:  u.IsVerified,
+	}, nil
+}
