57703aada4f988f5e1ab8606525212aaff683aa6
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2024-08-18 11:00:02 +0000

    store contains executable transaction function to start a transaction for sqlc

---
+++ sql/sqlc/store.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+func ExecTX(ctx context.Context, db *sql.DB, fn func(tx *sql.Tx) error) error {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := fn(tx); err != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			return rollbackErr
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
