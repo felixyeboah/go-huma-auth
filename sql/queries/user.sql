bb8d64c11c50236474c954999bbeda9eb775bfae
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2024-08-18 13:24:38 +0000

    auth service for registering user done

--- sql/queries/user.sql
+++ sql/queries/user.sql
@@ -2,13 +2,11 @@
 INSERT INTO users(
     name,
     email,
-    avatar,
     phone_number,
     password,
-    is_verified,
     role_id
 ) VALUES (
-    $1, $2, $3, $4, $5, $6, $7
+    $1, $2, $3, $4, $5
 ) RETURNING *;
 
 -- name: GetUserByEmail :one
